19d33a9493ad1b0884ce9623da657173938b5ad4
IgorDoring <idoring@tutanota.com>
2024-05-14 18:21:19 -0300

    creating and validating form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './components/card-form/card-form.component';
 
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const materialConponents = [
   MatFormFieldModule,
@@ -18,12 +19,18 @@ const materialConponents = [
   MatInputModule,
   MatCardModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
 ];
 
 @NgModule({
   declarations: [AppComponent, CardFormComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, materialConponents],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    materialConponents,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/card-form/card-form.component.ts
+++ src/app/components/card-form/card-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class CardFormComponent {
   blocks = [ "Amonkhet", "Ixalan", "Zendikar", "Ravnica", "Onslaught"] //blocos do select
+
+  cardForm = new FormGroup({
+    setName: new FormControl(''), //nome do set de carta
+    block: new FormControl('', Validators.required), //bloco que o set esta
+  })
+
+  get setName(){
+    return this.cardForm.get('setName')
+  }
+
+  get block(){
+    return this.cardForm.get('block')
+  }
+
+  constructor() { }
+
+  submit(){
+    this.setName!.setValue(this.setName!.value!.trim())
+    if(this.cardForm.valid){
+      console.log(this.cardForm.value)
+    }
+  }
 }
